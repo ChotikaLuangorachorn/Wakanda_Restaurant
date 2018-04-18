be95118c78d89f4f28c70a79676e771eaf493cd0
natchaneeya <natchaneeya.s@ku.th>
2018-04-18 23:23:33 +0700

    test

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'email', 'password', 'firstname', 'lastname'
+        'email', 'password', 'firstname', 'lastname', 'role'
     ];
 
     /**
