5e47cbbf837ff3cdbefb0c9acb6f4565a0a34c90
Chotika Luangorachorn <chotika.lu@ku.th>
2018-04-26 00:00:10 +0700

    table for show status of ordered

--- resources/views/customer/ordered.blade.php
+++ resources/views/customer/ordered.blade.php
@@ -12,25 +12,56 @@
 @section('content')
 <!-- ordered -->
 	<div class="row" id="btn-category" style="text-align: center;">
-		<div class="col-sm-6" style="background-color: red;">
-			<p>กำลังรอ</p>
+		<div class="col-sm-6">
+			<h4>กำลังรอ</h4>
 			<table class="table table-hover">
 				<thead>
-					<tr>
+					<tr class="table-light">
 				    	<th scope="col">รายการอาหาร</th>
 				    	<th scope="col">จำนวน</th>
+				    	<th scope="col">ใบเสร็จที่</th>
 				    </tr>
 				</thead>
 				<tbody>
-				    <tr class="table-active">
-				      	<td>Column content</td>
-				      	<td>Column content</td>
-				    </tr>
+					@foreach($receipts as $receipt)
+				    	@foreach($receipt->orders as $order)
+				    		@if($order->status==='wait')
+						    <tr class="table-primary">
+						      	<td>{{$order->menus->name}}</td>
+						      	<td>{{$order->amount}}</td>
+						      	<td>{{$receipt->id}}</td>
+						    </tr>
+						    @endif
+				    	@endforeach
+				    @endforeach
 			  	</tbody>
 			</table> 
 		</div>
-		<div class="col-sm-6" style="background-color: blue;">
-			<p>กำลังทำ</p>
+		<div class="col-sm-6" >
+			<h4>กำลังทำ</h4>
+			<table class="table table-hover">
+				<table class="table table-hover">
+				<thead>
+					<tr class="table-light">
+				    	<th scope="col">รายการอาหาร</th>
+				    	<th scope="col">จำนวน</th>
+				    	<th scope="col">ใบเสร็จที่</th>
+				    </tr>
+				</thead>
+				<tbody>
+					@foreach($receipts as $receipt)
+				    	@foreach($receipt->orders as $order)
+				    		@if($order->status==='cooking')
+						    <tr class="table-secondary">
+						      	<td>{{$order->menus->name}}</td>
+						      	<td>{{$order->amount}}</td>
+						      	<td>{{$receipt->id}}</td>
+						    </tr>
+						    @endif
+				    	@endforeach
+				    @endforeach
+			  	</tbody>
+			</table> 
 		</div>
 	</div>
 
