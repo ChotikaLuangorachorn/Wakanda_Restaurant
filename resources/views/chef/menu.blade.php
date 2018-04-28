cd15a1d29da31673750537c01cdc47fbf7e36a16
natchaneeya <natchaneeya.s@ku.th>
2018-04-28 21:39:28 +0700

    chef-can update status of food (sell or not sell)(complete!)

--- resources/views/chef/menu.blade.php
+++ resources/views/chef/menu.blade.php
@@ -35,11 +35,9 @@ i {
                                 <div class="card-header" >{{$menu->name}}</div>
                                 <div class="card-body">
                                 @if ($menu->image_path!='')
-                                    <div id='img-menu' style="width: 100%;height: 130px;background: url(/images/menu/{{$menu->image_path}});background-repeat: no-repeat;background-size: cover;background-position: center">
-                                    </div>
+                                    <div id='img-menu' style="width: 100%;height: 130px;background: url(/images/menu/{{$menu->image_path}});background-repeat: no-repeat;background-size: cover;background-position: center"></div>
                                 @else
-                                    <div id='img-menu' style="width: 100%;height: 130px;background: url(/images/menu/no_image.jpg);background-repeat: no-repeat;background-size: cover;background-position: center">
-                                    </div>
+                                    <div id='img-menu' style="width: 100%;height: 130px;background: url(/images/menu/no_image.jpg);background-repeat: no-repeat;background-size: cover;background-position: center"></div>
                                 @endif
 
                                 </div>
@@ -59,39 +57,42 @@ i {
 
 
     <div class="col-md-2"  style="text-align:center;">
-    <i class="fas fa-arrow-alt-circle-right th" onclick="$('#form1').submit();" style="font-size:100px; margin-top: 150px;"></i>
+    <i class="fas fa-arrow-alt-circle-right th" onclick="$('#form1').submit();" style="font-size:100px; margin-top: 160px;"></i>
+    <i class="fas fa-arrow-alt-circle-left th" onclick="$('#form2').submit();" style="font-size:100px; margin-top: 100px;"></i>
     </div>
 
 
     <div class="col-md-5">
-        <div class="card border-primary mb-3" style="max-width: 100%;" >
+        <div class="card border-secondary mb-3" style="max-width: 100%;" >
             <div class="card-header">Not sell</div>
             <div class="card-body">
+            <form id="form2" action="/chef/menus/{{$categoryNo}}" method="POST">
+                <div class="btn-group-toggle" data-toggle="buttons">
+                @csrf
+                @method('PUT')
                 @foreach($menus as $menu)
                     @if($categoryNo == $menu->category_id && $menu->status === 'not sell')
-                        <div class="card border-primary mb-3 .d-inline-block" style=" display: inline-block; width: 45%; margin-right: 2%;" >
-                            <div class="card-header" >{{$menu->name}}</div>
-                            <div class="card-body">
-                                @if ($menu->image_path!='')
-                                <div id='img-menu' style="width: 100%;height: 130px;background: url(/images/menu/{{$menu->image_path}});background-repeat: no-repeat;background-size: cover;background-position: center">
-                                    </div>
-                                @else
-                                    <div id='img-menu' style="width: 100%;height: 130px;background: url(/images/menu/no_image.jpg);background-repeat: no-repeat;background-size: cover;background-position: center">
-                                    </div>
-                                @endif
+                        <label for="menu{{$menu->id}}" class="btn btn-outline-secondary" style="display:inline-block;width: 45%;  margin-right: 2%; color:black">
+                            <div class="card border-secondary .d-inline-block">
+                                <div class="card-header" >{{$menu->name}}</div>
+                                <div class="card-body">
+                                    @if ($menu->image_path!='')
+                                        <div id='img-menu' style="width: 100%;height: 130px;background: url(/images/menu/{{$menu->image_path}});background-repeat: no-repeat;background-size: cover;background-position: center"></div>
+                                    @else
+                                        <div id='img-menu' style="width: 100%;height: 130px;background: url(/images/menu/no_image.jpg);background-repeat: no-repeat;background-size: cover;background-position: center"></div>
+                                    @endif
 
+                                </div>
                             </div>
-                        </div>
-
+                        <input hidden type="radio" name="menu" id="menu{{$menu->id}}" value="{{$menu->id}}">
+                        </label>
                     @endif
                 @endforeach
-
-                
-            </div>
-        </div>
+                </div>
+            </form>
       
+        </div>
     </div>
-</div>
 
 
 
