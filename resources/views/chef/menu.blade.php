5621a8a3ccc545af3a62939a733fc64389206fef
natchaneeya <natchaneeya.s@ku.th>
2018-04-28 15:37:21 +0700

    chef-can show menu by categories and status

--- resources/views/chef/menu.blade.php
+++ resources/views/chef/menu.blade.php
@@ -25,31 +25,25 @@ th {
             <div class="card-header">Sell</div>
                 <div class="card-body" >
                     @foreach($menus as $menu)
-                     
-                        <div class="card border-primary mb-3 .d-inline-block" style=" display: inline-block; width: 45%; margin-right: 2%;" >
-                            <div class="card-header">{{$menu->name}}</div>
-                            <div class="card-body">
-                            @if ($menu->image_path!='')
-						        <div id='img-menu' style="width: 100%;height: 150px;background: url(/images/menu/{{$menu->image_path}});background-repeat: no-repeat;background-size: cover;background-position: center">
-						        </div>
-						    @else
-						        <div id='img-menu' style="width: 100%;height: 150px;background: url(/images/menu/no_image.jpg);background-repeat: no-repeat;background-size: cover;background-position: center">
-						        </div>
-						    @endif
+                        @if($categoryNo == $menu->category_id && $menu->status === 'sell')
+                            <div class="card border-primary mb-3 .d-inline-block" style=" display: inline-block; width: 45%; margin-right: 2%;" >
+                                <div class="card-header" >{{$menu->name}}</div>
+                                <div class="card-body">
+                                @if ($menu->image_path!='')
+                                    <div id='img-menu' style="width: 100%;height: 130px;background: url(/images/menu/{{$menu->image_path}});background-repeat: no-repeat;background-size: cover;background-position: center">
+                                    </div>
+                                @else
+                                    <div id='img-menu' style="width: 100%;height: 130px;background: url(/images/menu/no_image.jpg);background-repeat: no-repeat;background-size: cover;background-position: center">
+                                    </div>
+                                @endif
 
-
+                                </div>
                             </div>
-                        </div>
+
+                        @endif
                     @endforeach
                 </div>
                     
-                    
-
-        
-
-                    
-
-
                         
                
         </div>
@@ -65,8 +59,26 @@ th {
         <div class="card border-primary mb-3" style="max-width: 100%;" >
             <div class="card-header">Not sell</div>
             <div class="card-body">
-                <h4 class="card-title">Primary card title</h4>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                @foreach($menus as $menu)
+                    @if($categoryNo == $menu->category_id && $menu->status === 'not sell')
+                        <div class="card border-primary mb-3 .d-inline-block" style=" display: inline-block; width: 45%; margin-right: 2%;" >
+                            <div class="card-header" >{{$menu->name}}</div>
+                            <div class="card-body">
+                                @if ($menu->image_path!='')
+                                <div id='img-menu' style="width: 100%;height: 130px;background: url(/images/menu/{{$menu->image_path}});background-repeat: no-repeat;background-size: cover;background-position: center">
+                                    </div>
+                                @else
+                                    <div id='img-menu' style="width: 100%;height: 130px;background: url(/images/menu/no_image.jpg);background-repeat: no-repeat;background-size: cover;background-position: center">
+                                    </div>
+                                @endif
+
+                            </div>
+                        </div>
+
+                    @endif
+                @endforeach
+
+                
             </div>
         </div>
       
